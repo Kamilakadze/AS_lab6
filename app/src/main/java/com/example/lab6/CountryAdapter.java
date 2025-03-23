fd7bb99e9642481e711d4532871ef8a3ea331389
kamila <dosymbekkamila@gmail.com>
2025-03-23 22:01:21 +0500

    adding countries

---
+++ app/src/main/java/com/example/lab6/CountryAdapter.java
@@ -0,0 +1,64 @@
+package com.example.lab6;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class CountryAdapter extends RecyclerView.Adapter<CountryAdapter.CountryViewHolder> {
+
+    private Context context;
+    private List<Country> countryList;
+
+    public CountryAdapter(Context context, List<Country> countryList) {
+        this.context = context;
+        this.countryList = countryList;
+    }
+
+    public static class CountryViewHolder extends RecyclerView.ViewHolder {
+        ImageView flagImageView;
+        TextView countryNameView;
+        TextView populationView;
+
+        public CountryViewHolder(View itemView) {
+            super(itemView);
+            flagImageView = itemView.findViewById(R.id.imageView_flag);
+            countryNameView = itemView.findViewById(R.id.textView_countryName);
+            populationView = itemView.findViewById(R.id.textView_population);
+        }
+    }
+
+    @NonNull
+    @Override
+    public CountryViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_row, parent, false);
+        return new CountryViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CountryViewHolder holder, int position) {
+        Country country = countryList.get(position);
+
+        holder.countryNameView.setText(country.getCountryName());
+        holder.populationView.setText("Population: " + country.getPopulation());
+
+        int imageResId = context.getResources().getIdentifier(
+                country.getFlagName(),
+                "mipmap",
+                context.getPackageName()
+        );
+        holder.flagImageView.setImageResource(imageResId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return countryList.size();
+    }
+}
--- app/src/main/java/com/example/lab6/MainActivity.java
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -2,23 +2,40 @@ package com.example.lab6;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    CountryAdapter adapter;
+    List<Country> countryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        countryList = getCountryData();
+
+        adapter = new CountryAdapter(this, countryList);
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<Country> getCountryData() {
+        List<Country> list = new ArrayList<>();
+
+        list.add(new Country("Vietnam", "vn", 98000000));
+        list.add(new Country("United States", "us", 320000000));
+        list.add(new Country("Russia", "ru", 142000000));
+
+        return list;
     }
 }
